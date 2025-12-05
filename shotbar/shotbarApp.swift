72a2b3fd2a69d872b5a24553630b4cced5a2e921
ibis <cassaicus+x@gmail.com>
2025-12-05 21:26:52 +0900

    Initial Commit

---
+++ shotbar/shotbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  shotbarApp.swift
+//  shotbar
+//
+//  Created by ibis on 2025/12/05.
+//
+
+import SwiftUI
+
+@main
+struct shotbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
